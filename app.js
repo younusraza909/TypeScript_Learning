160bc71880fbccb1515276a741867018eb6f644e
younusraza909 <younusraza909@gmail.com>
2022-04-03 23:44:30 +0500

    Basic Types For typescript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Your code goes here....');
+// Basic TypeScript Types
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 5;
+var n2 = 10.1;
+var result = add(n1, n2);
+console.log(result);
