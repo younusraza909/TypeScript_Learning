f70998b1e8afe35ac2419096a4259857a30087d7
younusraza909 <younusraza909@gmail.com>
2022-04-10 21:56:02 +0500

    generics finished

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -83,3 +83,65 @@ function extractAndConvert<
 console.log(
   extractAndConvert({ name: "Hello" }, "name")
 );
+
+// GENERIC CLASSES
+class DataStorage<
+  T extends string | boolean | number
+> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Max" });
+// objStorage.addItem({ name: "Manu" });
+// objStorage.removeItem({ name: "Max" });
+
+// console.log(objStorage.getItems());
+
+// *********************************************//
+// Generic Utility Types
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  // we have to convert in into coursegoal type bcz currently it is partail type
+  return courseGoal as CourseGoal;
+}
+
+const names_: Readonly<string[]> = [
+  "Max",
+  "Anna",
+];
+// names.push('Manu');
+// names.pop();
