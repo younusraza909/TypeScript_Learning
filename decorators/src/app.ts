0b015b810ee6504b472c50d1c9c81c9ef0ab49a0
younusraza909 <younusraza909@gmail.com>
2022-04-11 22:29:56 +0500

    multiple decorator execution

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -15,9 +15,32 @@ function Logger(logString: String) {
   };
 }
 
+// Another Example
+
+function WithTemplate(
+  template: string,
+  hookId: string
+) {
+  //undescore gives typescript hint that we are getting something but we are not interested in it
+  return function (constructor: any) {
+    const hookEl =
+      document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent =
+        p.name;
+    }
+  };
+}
+
 //decorator will exucte when class is define
 //we do not execute decorator while calling but in methd 2 we have to so outer function can ba called
+
+//decorator run bottom up last one will run first
+//function will run in same way but decorator in it will run bottom up
 @Logger("Logging-Person")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
