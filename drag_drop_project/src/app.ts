2a455a6a74adfa11dee8cf29b753151ea5665212
younusraza909 <younusraza909@gmail.com>
2022-04-13 22:18:47 +0500

    showing form from class

--- drag_drop_project/src/app.ts
+++ drag_drop_project/src/app.ts
@@ -1,1 +1,37 @@
 // Code goes here!
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    //here html is closed in template tag so we have to import it
+    this.templateElement =
+      document.getElementById(
+        "project-input"
+      )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(
+      "app"
+    )! as HTMLDivElement;
+
+    //it will import data from templte as as documentFragment
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element =
+      importedNode.firstElementChild as HTMLFormElement;
+
+    this.attatch();
+  }
+
+  private attatch() {
+    this.hostElement.insertAdjacentElement(
+      "afterbegin",
+      this.element
+    );
+  }
+}
+
+const prjInput = new ProjectInput();
