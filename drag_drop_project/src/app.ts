906ba54bbc71f8ec5e6c66b007ca5ab1033db6be
younusraza909 <younusraza909@gmail.com>
2022-04-13 22:50:53 +0500

    addding event listner

--- drag_drop_project/src/app.ts
+++ drag_drop_project/src/app.ts
@@ -4,33 +4,55 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     //here html is closed in template tag so we have to import it
-    this.templateElement =
-      document.getElementById(
-        "project-input"
-      )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById(
-      "app"
-    )! as HTMLDivElement;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
     //it will import data from templte as as documentFragment
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
-    this.element =
-      importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    //we have add id for element with just below code
+    this.element.id = "user-input";
 
+    //getting access to input field before attaching it to dom
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attatch();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
   private attatch() {
-    this.hostElement.insertAdjacentElement(
-      "afterbegin",
-      this.element
-    );
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
